62c4c1f07fb254cef67dff23f2df3fc9cd8c8a08
Fotios Papadopoulos <f.papadopoulos99@hotmail.com>
2020-11-16 14:12:04 +0200

    Remove

--- app/src/main/java/com/example/projecttechlog/REMOVE.java
+++ app/src/main/java/com/example/projecttechlog/REMOVE.java
@@ -10,5 +10,6 @@ public class REMOVE extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remove);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
