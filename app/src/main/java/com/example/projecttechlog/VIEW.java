98d2001c80cfd2e9b762007efeff2b95166ffc76
AceTsak <acetsak@gmail.com>
2020-11-16 13:33:23 +0200

    Δημιουργία button "Back" και υλοποίηση του στο να επιστρέφει στο main activity.

--- app/src/main/java/com/example/projecttechlog/VIEW.java
+++ app/src/main/java/com/example/projecttechlog/VIEW.java
@@ -3,12 +3,26 @@ package com.example.projecttechlog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class VIEW extends AppCompatActivity {
+public class VIEW extends AppCompatActivity
+    {
+    Button btnBack;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
+
+        Button btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                finish();
+            }
+        });
+        }
     }
-}
