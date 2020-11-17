be7bfa449ff8d35f49bf62a9d945f3852c1adac9
GodIuz <46162972+GodIuz@users.noreply.github.com>
2020-11-17 14:43:47 +0200

    EMPLOYEE JAVA CLASS

--- app/src/main/java/com/example/projecttechlog/EMPLOYEE.java
+++ app/src/main/java/com/example/projecttechlog/EMPLOYEE.java
@@ -2,13 +2,48 @@ package com.example.projecttechlog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EMPLOYEE extends AppCompatActivity {
+    Button exit,view;
+    Intent istart;
+
+    public Button view;
+    public Button exit = findViewById(R.id.buttonviewsch);
+
+    public EMPLOYEE(Object v) {
+        this.v = v;
+        view = findViewById(R.id.buttonviewsch);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee);
+    }
+    view.setOnClickListener(new View.OnClickListener()
+    private Object v;
+    {
+        public void onClick (View v)
+        {
+            OpenActivity(1);
+        }
+    }
+    private void OpenActivity(==0)
+    {
+        if (1 == 1)
+        {
+            istart = new Intent(this, VIEW.class);
+            startActivity(istart);
+        }
+        if (1 == 2)
+        {
+            finish();
+            System.exit(0);
+        }
     }
 }
